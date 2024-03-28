4ca76a36834f1400ca85ab69d8e85b786d6df8b0
Dai Hasegawa <dhasegawa@hgu.jp>
2024-03-28 17:24:13 +0900

    finish section 8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,8 @@ class ActiveSupport::TestCase
   #test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
   fixtures :all
 
-  # （すべてのテストで使うその他のヘルパーメソッドは省略）
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
