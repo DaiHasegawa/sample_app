59b042d84b15b6576c82495c81ba9711efaae452
Dai Hasegawa <dhasegawa@hgu.jp>
2024-03-28 19:19:34 +0900

    finish section 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
